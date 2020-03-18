8a7e2631a71de650750aad8a28eb7ca99a3d736c
Igor <i_gor88@mail.ru>
2020-03-18 20:13:02 +0600

    Исправление ошибок ДЗ1

--- Lesson01/L5.py
+++ Lesson01/L5.py
@@ -4,12 +4,13 @@
 revenue = int(input('Введите выручку: '))
 costs = int(input('Введите издержки: '))
 fin_result = revenue - costs
-if fin_result >= 0:
+if fin_result > 0:
 	print (f'Прибыль составляет {fin_result}.')
 	profitability = fin_result / revenue
 	print(f'Рентабельность составляет {profitability}.')
-	persons = int(input('Укажите численность сотрудников: '))
-	fin_for_person =  fin_result / persons
-	print(f'Прибыль в расчете на одного сотрудника составляет {intfin_for_person}.')
+
 else:
 	print (f'Убыток составляет {fin_result}.')
+persons = int(input('Укажите численность сотрудников: '))
+fin_for_person =  fin_result / persons
+print(f'Прибыль в расчете на одного сотрудника составляет {fin_for_person}.')
