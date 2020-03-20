3a0e25d5196ae00d8a760248782a68f50aa4b44f
Igor <i_gor88@mail.ru>
2020-03-20 20:29:06 +0600

    Улучшения кода

--- Lesson01/L2.py
+++ Lesson01/L2.py
@@ -6,10 +6,10 @@ if time > 3600:
 	hour = int(time / 3600)
 minut = int(((time - (hour * 3600)) / 60))	
 seconds = time - ((hour * 3600) + (minut * 60))
-if hour < 9:
-	hour = '0' + str(hour)
-if minut < 9:
-	minut = '0' + str(minut)
-if seconds < 9:
-	seconds = '0' + str(seconds)
-print (f'{hour}:{minut}:{seconds}')
+#if hour < 9:
+#	hour = '0' + str(hour)
+#if minut < 9:
+#	minut = '0' + str(minut)
+#if seconds < 9:
+#	seconds = '0' + str(seconds)
+print (f'{hour:02}:{minut:02}:{seconds:02}')
