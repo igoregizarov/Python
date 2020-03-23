ed4f877343d9baf99035f307307baec4d6a9f491
Igor <i_gor88@mail.ru>
2020-03-23 22:00:06 +0600

    Исправление ошибок ДЗ2

--- Lesson02/l5.py
+++ Lesson02/l5.py
@@ -7,5 +7,6 @@ elif n < my_list[-1]:
     my_list.append(n)
 else:
     i = my_list.index(n)
-    my_list.insert(i, n)
+    count = my_list.count(n)
+    my_list.insert(i + count, n)
 print(my_list)
